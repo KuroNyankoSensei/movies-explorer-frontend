0b14d5f120aad1fd25fcc4f26c4d99d5b278f7c4
Margarita Kolyasnikova <madnightmoon@gmail.com>
2022-10-07 17:25:36 +0500

    fix/ review comment 2

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,9 +6,9 @@ function Portfolio() {
         <section className="portfolio">
             <h3 className="portfolio__title">Портфолио</h3>
             <ul className="portfolio__links">
-                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/how-to-learn' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Статичный сайт</a><span>↗</span></li>
-                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/russian-travel' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Адаптивный сайт</a><span>↗</span></li>
-                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/react-mesto-auth' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Одностраничное приложение</a><span>↗</span></li>
+                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/how-to-learn' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Статичный сайт<span>↗</span></a></li>
+                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/russian-travel' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Адаптивный сайт<span>↗</span></a></li>
+                <li className="portfolio__link"><a href='https://github.com/KuroNyankoSensei/react-mesto-auth' target="_blank" rel="noopener noreferrer" className="portfolio__link-text">Одностраничное приложение<span>↗</span></a></li>
             </ul>
         </section>
     )
