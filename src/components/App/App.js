aabb39099384ab7ec5ea4e68386925fc2a178e05
Margarita Kolyasnikova <madnightmoon@gmail.com>
2022-10-10 21:32:55 +0500

    feat/ api tools

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,8 +11,12 @@ import { useState, useEffect } from 'react';
 import NavigationMobile from '../NavigationMobile/NavigationMobile';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import { currentUserContext } from '../../contexts/currentUserContext';
+import InfoToolTip from '../InfoToolTip/InfoToolTip';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState({});
 
   const [isOpenMobileMenu, setOpenMobileMenu] = useState(false);
 
@@ -37,51 +41,72 @@ function App() {
     return () => document.removeEventListener('keydown', handleCloseByEsc)
   });
 
+  const [isInfoToolTipOpen, setInfoToolTipOpen] = useState(false);
+
+  const [infoToolTipInformation, setInfoToolTipInformation] = useState({});
+
+  const handleInfoToolTip = (message, isGood) => {
+    setInfoToolTipInformation({ message, isGood });
+    setInfoToolTipOpen(true);
+  }
+
+  const handleCloseInfoToolTip = () => {
+    setInfoToolTipOpen(false);
+    setInfoToolTipInformation({});
+  }
+
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Header
-            page="main"
-          />
-          <Main />
-          <Footer />
-        </Route>
-        <Route path="/signup">
-          <Register />
-        </Route>
-        <Route path='/signin'>
-          <Login />
-        </Route>
-        <Route path="/profile">
-          <Header
-            onMenuButton={handleOpenMobileMenu}
-          />
-          <Profile />
-        </Route>
-        <Route path="/movies">
-          <Header
-            onMenuButton={handleOpenMobileMenu}
-          />
-          <Movies />
-          <Footer />
-        </Route>
-        <Route path="/saved-movies">
-          <Header
-            onMenuButton={handleOpenMobileMenu}
-          />
-          <SavedMovies />
-          <Footer />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-      <NavigationMobile
-        isOpen={isOpenMobileMenu}
-        onClose={handleCloseMobileMenu}
-      />
-    </div>
+    <currentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Header
+              page="main"
+            />
+            <Main />
+            <Footer />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path='/signin'>
+            <Login />
+          </Route>
+          <Route path="/profile">
+            <Header
+              onMenuButton={handleOpenMobileMenu}
+            />
+            <Profile />
+          </Route>
+          <Route path="/movies">
+            <Header
+              onMenuButton={handleOpenMobileMenu}
+            />
+            <Movies />
+            <Footer />
+          </Route>
+          <Route path="/saved-movies">
+            <Header
+              onMenuButton={handleOpenMobileMenu}
+            />
+            <SavedMovies />
+            <Footer />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+        <NavigationMobile
+          isOpen={isOpenMobileMenu}
+          onClose={handleCloseMobileMenu}
+        />
+        <InfoToolTip
+          isOpen={isInfoToolTipOpen}
+          onClose={handleCloseInfoToolTip}
+          info={infoToolTipInformation}
+        />
+      </div>
+    </currentUserContext.Provider>
   )
 }
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+export const MAIN_API_URL = 'https://api.kuronyankonoeiga.nomoredomains.sbs';
+export const SHORT_MOVIE_TIMING = 40;
+export const IMAGES_BASE_URL = 'https://api.nomoreparties.co';
